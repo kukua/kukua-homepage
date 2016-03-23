820e237ac2189aa4cf9963e56cfa6884197b39a9
Siebren Kranenburg <info@siebrenkranenburg.nl>
2016-03-23 15:22:44 +0100

    removed specific newsletter page for default template

--- public_html/wp-content/themes/template/Views/index/index.php
+++ public_html/wp-content/themes/template/Views/index/index.php
@@ -16,13 +16,7 @@
 <div class="default">
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-4">
-				<h2>Newsletter</h2>
-				<p>
-					Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters.
-				</p>
-			</div>
-			<div class="col-sm-6 col-sm-offset-2">
+			<div class="col-sm-12">
 				<?php the_content(); ?>
 			</div>
 		</div>
