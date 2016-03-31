5f4a824929d7dd601fd848b42ad960b25522c6e8
Siebren Kranenburg <info@siebrenkranenburg.nl>
2016-03-31 11:29:23 +0200

    added text for donate success / cancel

--- public_html/wp-content/themes/template/Views/partials/donate.php
+++ public_html/wp-content/themes/template/Views/partials/donate.php
@@ -3,7 +3,7 @@
 		<div class="row">
 			<div class="col-sm-5">
 				<h2><?= $title; ?></h2>
-				<p><?=$text; ?></p>
+				<p><?= $text; ?></p>
 			</div>
 			<div class="col-sm-5 col-sm-offset-2">
 				<h2>&nbsp;</h2>
@@ -13,6 +13,14 @@
 					<input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online.">
 					<img alt="" border="0" src="https://www.paypalobjects.com/nl_NL/i/scr/pixel.gif" width="1" height="1">
 				</form>
+				<?php if (isset($_GET["payment"])): ?>
+					<h2>&nbsp;</h2>
+					<?php if ($_GET["payment"] == "error"): ?>
+						<p class="text-muted"><i class="glyphicon glyphicon-remove"></i> The donation has been cancelled.</p>
+					<?php elseif ($_GET["payment"] == "success"): ?>
+						<p class="text-muted"><i class="glyphicon glyphicon-ok"></i> Thanks you for your donation!</p>
+					<?php endif; ?>
+				<?php endif; ?>
 			</div>
 		</div>
 	</div>
