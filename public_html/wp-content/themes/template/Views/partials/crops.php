bce549ff610091c2c9eb82fb93570f5b2b40241a
Siebren Kranenburg <info@siebrenkranenburg.nl>
2016-04-21 16:10:39 +0200

    added crops partial

---
+++ public_html/wp-content/themes/template/Views/partials/crops.php
@@ -0,0 +1,15 @@
+<div class="crops <?= isset($background) ? $background : ""; ?>">
+	<div class="container">
+		<div class="col-sm-10 col-sm-offset-1">
+			<h1>Via text message, Kukua can help farmers to make critical decisions about when to:</h1>
+			<ul>
+				<li>Plant</li>
+				<li>Insure their crops</li>
+				<li>Apply pesticide</li>
+				<li>Apply fertilizer</li>
+				<li>Harvest</li>
+				<li>Plant a different crop</li>
+			</ul>
+		</div>
+	</div>
+</div>
