95a098c61c0eafa42bb747f18c67739ad882e292
Siebren Kranenburg <info@siebrenkranenburg.nl>
2016-04-25 15:23:50 +0200

    disable user scaling on phones

--- public_html/wp-content/themes/template/Views/header.php
+++ public_html/wp-content/themes/template/Views/header.php
@@ -2,7 +2,8 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+
 	<title><?= $pageTitle ?></title>
 	<link href='https://fonts.googleapis.com/css?family=Asap:400,700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="<?= $base ?>/assets/dist/main.min.css">
